3004755a9f2893ceef20eec0c2caa194c659d096
scout b <scout.burghardt@gmail.com>
2018-07-31 19:17:14 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,7 +33,7 @@ def turn(board)
   input = gets.chomp
   index = input_to_index(input)
   while valid_move?(board, index) != true
-    puts "You are naughty. \nYou either did not enter a number between 1 and 9 or you are trying to overwrite an existing move."
+    puts "You are naughty. \nEither you did not enter a number between 1 and 9 or you are trying to overwrite an existing move."
     print "Tsk-tsk. Please try again: "
     input = gets.chomp
     index = input_to_index(input)
